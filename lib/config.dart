4efe120785b48bc1492249dd5888461c8b288e1f
patrickchow0803 <patrickchow0803@gmail.com>
2021-02-21 20:03:22 -0500

    Added a List of map to represent the categories of pets

---
+++ lib/config.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0xff416d6d);
+
+List<BoxShadow> listShadow = [
+  BoxShadow(color: Colors.grey[300], blurRadius: 30, offset: Offset(0, 10)),
+];
+
+List<Map<String, String>> categories = [
+  {'name': 'Cats', 'iconPath': 'images/cat.png'},
+  {'name': 'Dog', 'iconPath': 'images/dog.png'},
+  {'name': 'Horse', 'iconPath': 'images/horse.png'},
+  {'name': 'Parrot', 'iconPath': 'images/parrot.png'},
+  {'name': 'Rabbit', 'iconPath': 'images/rabbit.png'},
+];
